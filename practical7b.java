aa5dee2a5a5c53e50e376f30511ba817d7e15ee8
Bhushan <bhushansapkal207@gmail.com>
2026-05-14 19:48:22 +0530

    Initial commit

---
+++ practical7b.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; interface College { void showCollege(String collegeName); } interface
+Staff { void showStaff(String staffName); } class Student implements College, Staff { public void
+showCollege(String collegeName) { System.out.println("College: " + collegeName); } public void
+showStaff(String staffName) { System.out.println("Staff: " + staffName); } void
+showStudent(String studentName) { System.out.println("Student: " + studentName); } } public
+class practical7b { public static void main(String[] args) { Scanner sc = new Scanner(System.in);
+System.out.print("Enter College Name: "); String college = sc.nextLine(); System.out.print("Enter
+Staff Name: "); String staff = sc.nextLine(); System.out.print("Enter Student Name: "); String
+student = sc.nextLine(); // Polymorphism using interface reference College c = new Student(); //
+reference of interface Staff s = new Student(); // reference of interface Student st = new
+Student(); // actual object c.showCollege(college); // calls Student implementation
+s.showStaff(staff); // calls Student implementation st.showStudent(student); sc.close(); } }
